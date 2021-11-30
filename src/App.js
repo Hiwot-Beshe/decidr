33bc693260a1064eb3bd3ba25ccbdb2ff29d7066
Hiwot-Beshe <beshehiwot@gmail.com>
2021-11-30 11:34:01 -0800

    chore(add Item) add item to list container #8

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import React, { useState } from "react";
 
 function App() {
-  	const [listContainer, setListContainer] = useState([]);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
-  );
+	const [listContainer, setListContainer] = useState([]);
+	const [items, setItems] = useState("");
+	const [selectItem, setSelectItem] = useState("");
+  const onchange = (e) => setItems(e.target.value);
+  
+  const addItem = () => {
+		setListContainer([...listContainer, { v: listContainer }]);
+		setListContainer("");
+  };
+  	const submitHandler = (event) => {
+			event.preventDefault();
+			event.target.reset();
+    };
+  const selector = () => {
+		setSelectItem(listContainer[Math.floor(Math.random() * listContainer.length)]);
+	};
+	return (
+		<div className="App">
+			<header className="App-header">
+		
+				<h1>Decider</h1>
+			</header>
+      <div>
+       
+				<form onSubmit={submitHandler}>
+					<label>
+						List
+						<input type="text" name="name" onChange={onchange} />
+					</label>
+
+					<button onClick={addItem}>Add</button>
+				</form>
+			</div>
+		</div>
+	);
 }
 
 export default App;
