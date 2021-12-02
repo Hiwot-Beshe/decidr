16974b521e00fb95395b519b770cde528a9582b3
Hiwot-Beshe <beshehiwot@gmail.com>
2021-12-02 14:31:19 -0800

    feat(add display items): add function display lists  #11

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
 import AddItem from "./Components/AddItem";
+import SelectorItem from "./Components/SelectorItem";
+import DisplayItem from "./Components/DisplayItem";
 
-function App() {
-  const [listContainer, setListContainer] = useState([]);
-  console.log("List Container", listContainer);
-  return (
+function App(selectItem, setSelectItem) {
+	const [listContainer, setListContainer] = useState([]);
+	
+	return (
 		<div className="App">
 			<header className="App-header">
 				<p>Decidr</p>
 			</header>
 			<div>
 				<AddItem
+					listContainer={listContainer}
+					setListContainer={setListContainer}
+				/>
+			</div>
+			<div>
+				<DisplayItem
 					listContainer={listContainer}
 					setListContainer={setListContainer}
 				/>
