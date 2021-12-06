7cd3e6a70aa34602b5e12a0fd31ec1afaaeac15c
Hiwot-Beshe <beshehiwot@gmail.com>
2021-12-05 23:28:45 -0800

    feat(add message ) add message for list quanity

--- src/Components/SelectorItem.js
+++ src/Components/SelectorItem.js
@@ -1,66 +1,42 @@
 import { useState } from "react";
 
+export default function Selector({ listContainer, setListContainer }) {
+	const [selectItem, setSelectItem] = useState("");
+	// const winner = () => {
+	//     setSelectItem(listContainer[Math.floor(Math.random() * listContainer.length)]);
 
-export default function Selector({ listContainer, setListContainer, }) {
-    const [selectItem, setSelectItem] = useState("");
-    const winner = () => {
-        setSelectItem(listContainer[Math.floor(Math.random() * listContainer.length)]);
-		
-    };
-    return (
-			<div>
-				<div
-					className="selector-Wrap"
-					style={{
-						width: "50%",
-						height: " 5%",
-						marginLeft: "20%",
-						justifyContent: "space-between",
-						display: "flex",
-						boxSizing: "border-box",
-						alignItems: "center",
-						flexWrap: "wrap",
-						wordBreak: "break-all",
-						background:
-							"linear-gradient(-45deg, #EE7752, #DBDBAC,#ffff00,#d3d3d3,#DBDBAC ,#23A6, #DBDBAC)",
-					}}
-				>
-					<h2
-						id="selector-h2"
-						style={{
-							width: "50%",
-							height: " 5%",
-							marginLeft: "20%",
-							display: "flex",
-							boxSizing: "border-box",
-							alignItems: "center",
-							flexWrap: "wrap",
-							wordBreak: "break-all",
-							background:
-								"linear-gradient(-45deg,#DBDBAC,#23D5, #d3d3d3,#E73C,#DBDBAC )",
-						}}
-					>
-						{selectItem.v}
-					</h2>
-					<button
-						id="selector-btn"
-						onClick={winner}
-						style={{
-							width: "50%",
-							height: " 5%",
-							marginLeft: "20%",
-							display: "flex",
-							boxSizing: "border-box",
-							alignItems: "center",
-							flexWrap: "wrap",
-							wordBreak: "break-all",
-							background:
-								"linear-gradient(-45deg,#DBDBAC,#23D5, #d3d3d3,#E73C,#DBDBAC )",
-						}}
-					>
+	// };
+	const winner = () => {
+		const selectItem = Math.floor(Math.random() * listContainer.length);
+		if (listContainer.length <= 1) {
+			setSelectItem("");
+			alert("The list must contain at least 2 items");
+		} else if (listContainer.length > 1) {
+			setSelectItem(listContainer[selectItem]);
+		}
+	};
+
+	return (
+		<div>
+			<div className="selector-Wrap">
+				{listContainer.length <= 1 ? (
+					<h5 id="selector-h2">
+						A minimum of two items is
+						required for the get
+						Selector item to work.
+					</h5>
+				) : (
+					""
+				)}
+				<h2 id="selector-h2">{selectItem.v}</h2>
+				{winner.length ? (
+					<h2 className="selectedItem">{selectItem.v}</h2>
+				) : (
+					<button id="selector-btn" onClick={winner}>
 						Selector Item
 					</button>
-				</div>
+				)}
 			</div>
-		);
+		</div>
+	);
 }
